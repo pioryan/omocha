0a40c03e4f1b4439e24b43ccf5eab5abadd49cff
Pio Ryan <pioryan@gmail.com>
2012-08-11 23:15:18 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omocha::Application.config.secret_token = '65bebdebb566485b47cf887ec2436dd2ad0d9e381d66d568150ee4429fd6a29c149ebc5920ee99e0152c597a2618adcfff1877669830421bc92d5f063751f610'
