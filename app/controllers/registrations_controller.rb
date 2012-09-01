12ff520cd60355b7cafa92583e447e74278bc4b4
Ardee Aram <ardee.aram@gmail.com>
2012-09-01 17:24:12 +0800

    Fix Devise Recaptcha redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,16 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
   def create
+    if verify_recaptcha
+      super
+    else
+      build_resource
+      clean_up_passwords(resource)
+      flash[:alert] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
+      render "users/registrations/new"
+    end
+  end
+  
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Omocha::Application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
-  devise_for :users
+  devise_for :users,  :controllers => { :registrations => "registrations" }
   devise_scope :user do
     get "users/sign_out",:to => "devise/sessions#destroy"
   end
+  
+        
   root :to => "home#index"
 end
